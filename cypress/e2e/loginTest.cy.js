9de78783bb04ce15dfe34ac2919d0bfaba0cd134
Tatiana Borisova <taniaborisovafr@gmail.com>
2024-09-23 14:08:37 +0200

    requête Get pour afficher la fiche du produit selectionné

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,23 +1,17 @@
-describe('Registration Form Test', () => {
-  it('Should fill the registration form and be redirected on success', () => {
+describe("Registration Form Test", () => {
+  it("Should fill the registration form and be redirected on success", () => {
+    cy.intercept("POST", "**/login").as("loginRequest");
 
-    cy.intercept('POST', '**/login').as('loginRequest');
+    cy.visit("http://localhost:8080/#/login");
 
-    cy.visit('http://localhost:8080/#/login');
+    cy.get('[data-cy="login-input-username"]').type("test2@test.fr");
 
-
-    cy.get('[data-cy="login-input-username"]')
-      .type('test2@test.fr');
-
-
-    cy.get('[data-cy="login-input-password"]')
-      .type('testtest');
+    cy.get('[data-cy="login-input-password"]').type("testtest");
 
     cy.get('[data-cy="login-submit"]').click();
 
-    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
+    cy.wait("@loginRequest").its("response.statusCode").should("eq", 200);
 
-
-    cy.url().should('eq', 'http://localhost:8080/#/');
+    cy.url().should("eq", "http://localhost:8080/#/");
   });
 });
---
+++ cypress/e2e/product/sheetTest.cy.js
@@ -0,0 +1,29 @@
+describe("API Testing with Dynamic Product Selection", () => {
+  it("Should send a GET request for the selected product when navigating to its page", () => {
+    cy.intercept("GET", /http:\/\/localhost:8081\/products\/\d+/, {
+      statusCode: 200,
+      body: (req) => {
+        const id = req.url.split("/").pop();
+        return {
+          id: parseInt(id),
+          name: `Product ${id}`,
+        };
+      },
+    }).as("getProduct");
+
+    cy.visit("http://localhost:8080/products");
+
+    cy.get('[data-cy="product-home-link"]')
+      .its("length")
+      .then((length) => {
+        const randomIndex = Math.floor(Math.random() * length);
+        cy.get('[data-cy="product-home-link"]').eq(randomIndex).click();
+      });
+
+    cy.wait("@getProduct").then((interception) => {
+      console.log(interception);
+      expect(interception).to.exist;
+      expect(interception.response.statusCode).to.eq(200);
+    });
+  });
+});
