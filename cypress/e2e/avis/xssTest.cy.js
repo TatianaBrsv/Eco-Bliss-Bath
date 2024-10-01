fc2442a65630ef846f1cd50b428a7158f70f03e2
Tatiana Borisova <taniaborisovafr@gmail.com>
2024-10-01 18:04:30 +0200

    TestXss a été modifé

--- cypress/e2e/avis/xssTest.cy.js
+++ cypress/e2e/avis/xssTest.cy.js
@@ -1,7 +1,7 @@
 describe("Requête pour tester la vulnérabilité XSS lors de l'envoi d'un avis", () => {
   before(() => {
     const testToken =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MjczNjA1MTksImV4cCI6MTcyNzM2NDExOSwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoidGVzdDJAdGVzdC5mciJ9.O-37foW8C0Ho9nPcfqhoKTu_V13yvN3XryyoGxJ5AKviV3YkDfloosUqNj5-f9jfncXpnSQroGIDss4tzvaUB7RlSOXpZeZ_n5YlwhI2-6hl_Y_-swS10HnNY_uVgyzX096F-duk6V0jr7HUi8n2guPhsk4oI_nJJOiooOcNxP84IJuFJL8CiRMGrxSr2_0HCAk7mG7cb3jUra-cI2JHwjjLUPjiXta-Oe4H-hbm-o7fozIBmdofn-FueLmkfxBzRq5044MLBW6IUvoMwTk43xcHNP7QiL2stVD2OsaV-SPOtg4LgNF9RGNDjS_o1f6gHWHLzJ5Hbbe7elW14CWn0A";
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3Mjc3ODUxNzgsImV4cCI6MTcyNzc4ODc3OCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoidGVzdDJAdGVzdC5mciJ9.hDD8vlg2agyyZ70aTe4CUiIuUqEws49ImfC761VeXGchrviLVCVcbYMocaQOB4vkc7S5MGtss840F5uf-j8QdmI8BeqJq8nvLWAXyK2ItRgKwFhQh7DgVMM2Ltgf_blg8gnScElO__WjifOBaztkpTvuRZ9MR25ww5F3MdcV54l5eSqsZ5HQL8PMdaTPaPWxm1rBK4Bjcx49kkW1Ko-MGg-MeZsAE8XJh2zSlcmxJlkmCoGSy-opODoFkva6lVx7LTFmRxFbY0MDBnoWIHYaMhgYxqiVvwOgYgyPN_HC269GNIIIdc0vX-AIFc-3L_3YYCzD5KDFQc-zG4WfvqYDtg";
     window.localStorage.setItem("user", testToken);
   });
 
@@ -33,14 +33,9 @@ describe("Requête pour tester la vulnérabilité XSS lors de l'envoi d'un avis"
     cy.wait("@postReview").then((interception) => {
       expect(interception.response.statusCode).to.eq(200);
 
-      cy.get(".review-section").should(
-        "contain",
-        "&lt;script&gt;alert('XSS')&lt;/script&gt;"
-      );
-      cy.get(".review-section").should(
-        "contain",
-        "&lt;img src=x onerror=alert('XSS')&gt;"
-      );
+      cy.get(".review-section").should("not.contain", "<script>alert('XSS')</script>"); // Vérification que le code XSS ne s'exécute pas
+      cy.get(".review-section").should("contain", "&lt;script&gt;alert('XSS')&lt;/script&gt;"); // Vérification que le code s'affiche en tant que texte
+  
     });
   });
 });
